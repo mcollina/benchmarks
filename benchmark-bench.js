fd0853a56ae7c4e9fea77a94001e58390446724d
Eran Hammer <eran@hammer.io>
2017-12-14 02:26:54 -0800

    Batch script

--- benchmark-bench.js
+++ benchmark-bench.js
@@ -1,143 +1,21 @@
 #!/usr/bin/env node
 'use strict'
 
-const inquirer = require('inquirer')
 const bench = require('./lib/bench')
 
-function select (callback) {
-  inquirer.prompt([
-    {
-      type: 'checkbox',
-      message: 'Select packages',
-      name: 'list',
-      choices: [
-        new inquirer.Separator(' = The usual ='),
-        {
-          name: 'fastify',
-          checked: true
-        },
-        {
-          name: 'express'
-        },
-        {
-          name: 'koa'
-        },
-        {
-          name: 'hapi'
-        },
-        {
-          name: 'connect'
-        },
-        {
-          name: 'restify'
-        },
-        {
-          name: 'take-five'
-        },
-        {
-          name: 'total.js'
-        },
-        new inquirer.Separator(' = The extras = '),
-        {
-          name: 'connect-router'
-        },
-        {
-          name: 'express-route-prefix'
-        },
-        {
-          name: 'express-with-middlewares'
-        },
-        {
-          name: 'koa-router'
-        },
-        {
-          name: 'fastify-big-json'
-        },
-        {
-          name: 'micro'
-        },
-        {
-          name: 'micro-router'
-        },
-        {
-          name: 'trek-engine'
-        },
-        {
-          name: 'trek-engine-router'
-        }
-      ],
-      validate: function (answer) {
-        if (answer.length < 1) {
-          return 'You must choose at least one package.'
-        }
-        return true
-      }
+async function run() {
+
+    const intervals = [10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+    for (let i = 0; i < intervals.length; ++i) {
+        const options = {
+            connections: intervals[i],
+            duration: 40,
+            pipelining: 10
+        };
+
+        console.log('Connection:', intervals[i]);
+        await bench(options, ['bare', 'express', 'hapi', 'koa', 'restify', 'fastify']);
     }
-  ])
-  .then(function (answers) {
-    callback(answers.list)
-  })
 }
 
-inquirer.prompt([
-  {
-    type: 'confirm',
-    name: 'all',
-    message: 'Do you want to run all benchmark tests?',
-    default: false
-  },
-  {
-    type: 'input',
-    name: 'connection',
-    message: 'How many connection you need?',
-    default: 100,
-    validate (value) {
-      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
-    },
-    filter: Number
-  },
-  {
-    type: 'input',
-    name: 'pipelining',
-    message: 'How many pipelining you need?',
-    default: 10,
-    validate (value) {
-      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
-    },
-    filter: Number
-  },
-  {
-    type: 'input',
-    name: 'duration',
-    message: 'How long does it takes?',
-    default: 5,
-    validate (value) {
-      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
-    },
-    filter: Number
-  }
-]).then((opts) => {
-  if (!opts.all) {
-    select(list => bench(opts, list))
-  } else {
-    bench(opts, [
-      'bare',
-      'connect',
-      'connect-router',
-      'express-route-prefix',
-      'express-with-middlewares',
-      'express',
-      'hapi',
-      'koa-router',
-      'koa',
-      'restify',
-      'take-five',
-      'total.js',
-      'fastify',
-      'micro',
-      'micro-router',
-      'trek-engine',
-      'trek-engine-router'
-    ])
-  }
-})
+run();
